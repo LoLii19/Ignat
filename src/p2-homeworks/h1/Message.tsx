2b120f7f32b7221d691bfca5f7e417ab3ccf4f56
LoLii19 <cozlowskaya2010@yandex.ru>
2022-11-08 20:28:41 +0300

    Added id to HTML

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -12,11 +12,11 @@ type MessagePropsType ={
 function Message(props: MessagePropsType) {
     return (
         <div className={classes.border}>
-           <img className={classes.img} id={"#hw1-avatar-1"} src={props.avatar}/>
+           <img className={classes.img} id={"hw1-avatar-1"} src={props.avatar}/>
             <div className={classes.bordertext}>
-            <div className={classes.name}>{props.name} id={'#hw1-name-1'}</div>
-            <div className={classes.message}>{props.message} id={'#hw1-text-1'} </div>
-            <div className={classes.time}>{props.time} id={'#hw1-time-1'}</div>
+            <div className={classes.name}>{props.name} id={'hw1-name-1'}</div>
+            <div className={classes.message}>{props.message} id={'hw1-text-1'} </div>
+            <div className={classes.time}>{props.time} id={'hw1-time-1'}</div>
         </div>
         </div>
     )
